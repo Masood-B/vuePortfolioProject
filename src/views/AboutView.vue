8330fcc1d8738b4fd8e630ae9b152f7b40148f16
Masood-B <masoodbasardien123@gmail.com>
2023-07-01 13:04:20 +0200

    structure done

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h2 class="display-2">About</h2>
   </div>
 </template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/ProjectView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2 class="display-2">Project</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
