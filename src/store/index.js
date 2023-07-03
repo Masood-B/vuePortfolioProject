b12b1905da1954a0702cd4f0672a368fda9f86c3
Masood-B <masoodbasardien123@gmail.com>
2023-07-03 16:46:08 +0200

    push

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,58 @@
 import { createStore } from 'vuex'
-
+const dataURL = "https://masood-b.github.io/vueJsonPortfolioProject/"
 export default createStore({
   state: {
+    education: null,
+    skill: null,
+    project: null
   },
   getters: {
   },
   mutations: {
+    setEducation (state, dataEducation){
+      state.education = dataEducation
+    },
+    setSkill (state, dataSkill){
+      state.skill = dataSkill
+    },
+    setProject (state, dataProject){
+      state.project = dataProject
+    }
   },
   actions: {
+    async fetchEducation (context){
+      try{
+        let book = await fetch(dataURL);
+        let {Education} = await book.json();
+        if (Education){
+          context.commit('setEducation', Education);
+        }
+      }catch (event){
+        console.log(event.message);
+      }
+    },
+    async fetchSkill (context){
+      try{
+        let talent = await fetch (dataURL);
+        let {Skills} = await talent.json();
+        if (Skills){
+          context.commit('setSkill', Skills);
+      }
+    }catch(event){
+      console.log(event.message);
+    }
+    },
+    async fetchProject (context){
+      try{
+        let prototype = await fetch(dataURL);
+        let {Projects} = await prototype.json();
+        if (Projects){
+          context.commit('setProject', Projects);
+        }
+      }catch(event){
+        console.log(event.message);
+      }
+    }
   },
   modules: {
   }
--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -1,15 +1,41 @@
 <template>
     <div>
         <h2 class="display-2">Project</h2>
+        <div class="card" v-for="data in project" :key="data.id">
+            <img :src="data.background" alt="..." loading="lazy">
+            <div class="card-content">
+              <h2 class="card-title">
+               {{ data.title }}
+              </h2>
+              <a :href="data.githublink" target="_blank">
+                Github
+              </a>
+              <a :href="data.netlifylink" target="_blank">
+                Netlify
+            </a>
+            </div>
+          </div>
     </div>
 </template>
 
 <script>
     export default {
-        
+        computed:{
+        project(){
+            return this.$store.state.project
+        }
+       },
+       mounted() {
+        this.$store.dispatch('fetchProject')
+       }
     }
 </script>
 
 <style scoped>
-
+img{
+    width:10rem;
+}
+.card{
+    width:20rem;
+}
 </style>
