3fb962eb16ba2ae41ac88570bf0fad01f2fe080e
Masood-B <masoodbasardien123@gmail.com>
2023-07-05 15:10:46 +0200

    content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,7 +6,8 @@
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
         <h2 class="featurette-heading fw-normal lh-1">Oh yeah, it’s that good. <span class="text-body-secondary">See for yourself.</span></h2>
-        <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+        <p class="lead">I'm a guy who is 21 year at age. I live in Mitchell's Plain where i attended Meadowridge Primary School and went to Spine Road High School during my young days. I went to Cape Peninsula University of Technology for 3 years but never got to finished it. Now im following a dream that i had as a kid, to have influence over the game world I want to make.</p>
+        <p class="lead">I'm the type of person who's motivated and driven to be a great person someday. I'm not afraid to take a risk to learn new stuff. I always like to interact with people that would help me become a better version of myself. I love reading books, learning new technology, doing exercises and playing games. Also, I really do enjoy spend time with myself. To clear unwanted thought that might affect me.</p>
       </div>
       <div class="col-md-5 order-md-1">
         <img src="https://i.postimg.cc/wvd0VDyV/frontimage.jpg" alt="">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,29 @@
 <template>
   <div>
-    <h2 class="display-2">Home</h2>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading fw-normal lh-1">Oh yeah, it’s that good. <span class="text-body-secondary">See for yourself.</span></h2>
-        <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
-      </div>
-      <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"/><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg>
-      </div>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-      <div class="col-md-7">
-        <h2 class="featurette-heading fw-normal lh-1">And lastly, this one. <span class="text-body-secondary">Checkmate.</span></h2>
-        <p class="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
-      </div>
-      <div class="col-md-5">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"/><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg>
+  <h2 class="display-2" >Home</h2>
+    <div class="container col-xxl-8 px-4 py-5">
+      <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+        <div class="col-10 col-md-8 col-lg-4">
+          <img src="https://i.postimg.cc/wvd0VDyV/frontimage.jpg" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+        </div>
+        <div class="animate-wrapper col-10 col-md-8 col-lg-8">
+          <div class="animated-title">
+            <div class="text-top">
+              <div>
+                <span>I am Masood Basardien</span>
+                <span>Im pursuing my passion of becoming a game developer</span>
+              </div>
+            </div>
+            <div class="text-bottom">
+              <div><button type="button" class="btn btn-secondary btn-lg px-4 me-md-2"><router-link to="/about">About Me</router-link></button></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-
-    <hr class="featurette-divider">
-
   </div>
+  
+
 </template>
 
 <script>
@@ -38,5 +33,68 @@
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css?family=Roboto:700');
+@keyframes showTopText {
+  0% { transform: translate3d(0, 100%, 0); }
+  40%, 60% { transform: translate3d(0, 50%, 0); }
+  100% { transform: translate3d(0, 0, 0); }
+}
+@keyframes showBottomText {
+  0% { transform: translate3d(0, -100%, 0); }
+  100% { transform: translate3d(0, 0, 0); }
+}
+.animate-wrapper {
+  position: relative;
+}
+.animated-title {
+  color: #222;
+  font-family: Roboto, Arial, sans-serif;
+  height: 50vmin;
+  left: 50%;
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 50rem;
+}
+.animated-title > div {
+  height: 50%;
+  overflow: hidden;
+  position: absolute;
+  width: 100%;
+}
+.animated-title > div div {
+  font-size: 2rem;
+  padding: 1rem 0;
+  position: absolute;
+}
+.animated-title > div div span {
+  display: block;
+}
+.animated-title > div.text-top {
+  border-bottom: 1vmin solid #000;
+  top: 0;
+}
+.animated-title > div.text-top div {
+  animation: showTopText 1s;
+  animation-delay: 0.5s;
+  animation-fill-mode: forwards;
+  bottom: 0;
+  transform: translate(0, 100%);
+}
+.animated-title > div.text-top div span:first-child {
+  color: #767676;
+}
+.animated-title > div.text-bottom {
+  bottom: 0;
+}
+.animated-title > div.text-bottom div {
+  animation: showBottomText 0.5s;
+  animation-delay: 1.75s;
+  animation-fill-mode: forwards;
+  top: 0;
+  transform: translate(0, -100%);
+}
+@media (width < 1230px) {
 
+}
 </style>
