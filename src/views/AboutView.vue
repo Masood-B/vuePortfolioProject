aa4cbdbb1ac6117e6f12f0edd29dc5ba95a4ef68
Masood-B <masoodbasardien123@gmail.com>
2023-11-23 09:40:12 +0200

    change responsiveness

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,8 +2,9 @@
   <div class="about-content p-2">
     <div class="container">
       <div class="row">
-        <div class="col-md-6">
-          <img class="profile-img animate__animated animate__bounceInDown animate__delay-2"
+        <div class="col-md-6 d-none d-md-block">
+          <img
+            class="profile-img animate__animated animate__bounceInDown animate__delay-2"
             src="https://i.postimg.cc/wvd0VDyV/frontimage.jpg"
             alt=""
             loading="lazy"
@@ -13,26 +14,23 @@
 
         <div class="col-md-6">
           <p class="lead about-me animate__animated animate__backInRight animate__delay-1s">
-            I'm a guy who is 21 year at age. I live in Mitchell's Plain where i
-            attended Meadowridge Primary School and went to Spine Road High
-            School during my young days. I went to Cape Peninsula University of
-            Technology for 3 years but never got to finished it. Now im
-            following a dream that i had as a kid, to have influence over the
-            game world I want to make.
+            I'm a 21-year-old guy living in Mitchell's Plain. I attended
+            Meadowridge Primary School and Spine Road High School in my younger
+            days. Although I spent 3 years at Cape Peninsula University of
+            Technology, I didn't finish. Now, I'm pursuing my childhood dream
+            of influencing the game world.
           </p>
           <p class="lead about-me animate__animated animate__backInRight animate__delay-2s">
-            I'm the type of person who's motivated and driven to be a great
-            person someday. I'm not afraid to take a risk to learn new stuff. I
-            always like to interact with people that would help me become a
-            better version of myself. I love reading books, learning new
-            technology, doing exercises and playing games. Also, I really do
-            enjoy spend time with myself. To clear unwanted thought that might
-            affect me.
+            I'm motivated and driven to become a great person. I'm not afraid
+            to take risks and learn new things. I enjoy interacting with people
+            who help me become a better version of myself. I love reading
+            books, learning new technology, exercising, playing games, and
+            spending time alone to clear unwanted thoughts.
           </p>
 
           <div class="row second animate__animated animate__lightSpeedInRight animate__delay-3s">
             <div class="col-md-4">
-              <div class="card " style="width: 15rem">
+              <div class="card" style="width: 100%">
                 <img
                   src="https://i.postimg.cc/76wj59ZX/top-10-books-every-college-student-read-1024x640.jpg"
                   class="card-img-top img-fluid"
@@ -42,7 +40,7 @@
                 />
                 <div class="card-body">
                   <h5 class="card-title">Books</h5>
-                  <p class="card-text">Mysteries</p>
+                  <p class="card-text">Mystery</p>
                   <p class="card-text">Romance</p>
                   <p class="card-text">Fiction</p>
                 </div>
@@ -50,7 +48,7 @@
             </div>
 
             <div class="col-md-4">
-              <div class="card" style="width: 15rem">
+              <div class="card" style="width: 100%">
                 <img
                   src="https://i.postimg.cc/CKVQ27kY/y5dxp8yemlzc8bypgfsl.jpg"
                   class="card-img-top"
@@ -60,7 +58,7 @@
                 />
                 <div class="card-body">
                   <h5 class="card-title">Games</h5>
-                  <p class="card-text">Horror</p>
+                  <p class="card-text">MOBA</p>
                   <p class="card-text">JRPG</p>
                   <p class="card-text">Visual Novel</p>
                 </div>
@@ -68,7 +66,7 @@
             </div>
 
             <div class="col-md-4">
-              <div class="card" style="width: 15rem">
+              <div class="card" style="width: 100%">
                 <img
                   src="https://i.postimg.cc/MpBLtNcd/adobestock-4437974.jpg"
                   class="card-img-top"
@@ -97,43 +95,44 @@ export default {};
 
 <style scoped>
 .about-me {
-  color: white;
+  color: #080000;
 }
 img {
   width: 100%;
 }
 p {
-  color: black;
+  color: #080000;
 }
-h5{
+h5 {
   text-decoration: underline;
 }
 
-.card{
-  width: 13rem !important;
-  background:orange;
+.card {
+  width: 100%;
+  background: orange;
   border: 1px solid black;
 }
 
-@media screen and (max-width: 760px) {
-  .second{
+@media screen and (max-width: 991px) {
+  .second {
     width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
     gap: 3rem;
   }
-  
-  .col-md-4{
+
+  .col-md-4 {
     width: 100%;
     height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
-    
+    margin-left: 0;
   }
 }
-.profile-img{
+
+.profile-img {
   border: 5px solid #1b9dc5;
   padding: 2px;
   border-radius: 50%;
@@ -141,5 +140,23 @@ h5{
   border-left-color: #cf741f;
   width: 100%;
   margin-top: 5rem;
+}
+
+@media screen and (max-width: 992px) {
+  .col-md-6 {
+    width: 100%;
+  }
+
+  .profile-img {
+    display: none;
+
+    
+  }
+
+  .second .col-md-4 {
+    width: 50%;
+    padding-top: 1rem;
+  }
+ 
 }
 </style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,101 +1,101 @@
 <template>
     <div class="con p-2">
-            <div class="container p-4 animate__animated animate__fadeInLeftBig animate__delay-2">
-                <div class="row">
-                    <div id="form_left" class="col-md-6 ">
-                        <form action="https://formspree.io/f/xbjewvba" method="POST">
-                            <div class="mb-3">
-                                <label for="exampleFormControlInput1" name="fullname" class="form-label">Full Name</label>
-                                <input name="name" type="name" class="form-control" id="exampleFormControlInput1" placeholder="Your Name"  required
-                                oninvalid="this.setCustomValidity('You need to put your name here')"
-                                oninput="this.setCustomValidity('')">
-                                <label for="exampleFormControlInput2" name="email" class="form-label">Email address</label>
-                                <input name="email" type="email" class="form-control" id="exampleFormControlInput2" placeholder="Your Email"  required
-                                oninvalid="this.setCustomValidity('You need to put your email here')"
-                                oninput="this.setCustomValidity('')">
-                              </div>
-                              <div class="mb-3">
-                                <label for="exampleFormControlTextarea1" name="message" class="form-label" >Message</label>
-                                <textarea class="form-control" name="message" id="exampleFormControlTextarea1" rows="3" placeholder="Write Here"  required
-                                oninvalid="this.setCustomValidity('You need to write here')"
-                                oninput="this.setCustomValidity('')"></textarea>
-                              </div>
-                              <div>
-                                <button class="btn btn-info">Submit</button>
-                              </div>
-                        </form>
-                    </div>
-                    <div class="col-md-1">
-
-                    </div>
-                    <div id="form_right" class="col-md-5">
-                        <p class="contact-text"><i class="bi bi-telephone"></i> 079 298 0631</p>
-                        <p class="contact-text"><i class="bi bi-envelope"></i> masoodbasardien123@gmail.com</p>
-
-                        <hr>
-                        
-                        <div class="media">
-                            <ul class="d-flex justify-content-center">
-                                <li><a href="https://www.linkedin.com/in/masood-basardien-6aa759282/" target="_blank" class="icon"><i class="bi bi-linkedin"></i></a></li>
-                                <li><a href="https://github.com/Masood-B?tab=repositories" target="_blank" class="px-3"><i class="bi bi-github"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+      <div class="container p-4 animate__animated animate__fadeInLeftBig animate__delay-2">
+        <div class="row">
+          <div id="form_left" class="col-md-6">
+            <form action="https://formspree.io/f/xbjewvba" method="POST">
+              <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Full Name</label>
+                <input name="name" type="name" class="form-control" id="exampleFormControlInput1" placeholder="Your Name" required oninvalid="this.setCustomValidity('You need to put your name here')" oninput="this.setCustomValidity('')"/>
+                <label for="exampleFormControlInput2" class="form-label">Email address</label>
+                <input name="email" type="email" class="form-control" id="exampleFormControlInput2" placeholder="Your Email" required oninvalid="this.setCustomValidity('You need to put your email here')" oninput="this.setCustomValidity('')"/>
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+                <textarea class="form-control" name="message" id="exampleFormControlTextarea1" rows="3" placeholder="Write Here" required oninvalid="this.setCustomValidity('You need to write here')" oninput="this.setCustomValidity('')"></textarea>
+              </div>
+              <div>
+                <button type="submit" class="btn btn-info">Submit</button>
+              </div>
+            </form>
+          </div>
+          <div id="form_right" class="col-md-6 d-flex flex-column align-items-center justify-content-center py-3">
+            <div class="text-center">
+              <p class="contact-text">
+                <i class="bi bi-telephone"></i> 079 298 0631
+              </p>
+              <p class="contact-text">
+                <i class="bi bi-envelope"></i> masoodbasardien123@gmail.com
+              </p>
             </div>
-            
-</div>
-</template>
-
-<script>
-    export default {
-        
-    }
-</script>
-
-<style scoped>
-#form_left {
-    margin-top: 7rem;
-}
-#form_right {
-    margin-top: 11rem;
-}
-@media (width < 780px) {
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {};
+  </script>
+  
+  <style scoped>
+  #form_left {
+    margin-top: 2rem;
+  }
+  
+  #form_right {
+    margin-top: 2rem;
+  }
+  
+  @media (max-width: 768px) {
     #form_left {
-        margin-top: 0;
+      margin-top: 0;
     }
+  
     #form_right {
-        margin-top: 0;
+      margin-top: 0;
     }
-}
-.container{
+  
+    .btn-info {
+      width: 100%;
+    }
+  }
+  
+  .container {
     background: orange;
-    height: 70vh;
+    height: auto;
     margin: auto;
     margin-top: 1rem;
     margin-bottom: 1rem;
-}
-.btn-info{
-    width:50%;
+    padding: 2rem;
+  }
+  
+  .btn-info {
     letter-spacing: 2px;
     background: brown;
     transition: all 0.5s ease-in-out;
     border: 1px solid black;
     color: white;
-}
-.btn-info:hover{
+  }
+  
+  .btn-info:hover {
     transform: scale(1.1);
-}
-.contact-text{
-    font-size:17px;
-}
-ul{
+  }
+  
+  .contact-text {
+    font-size: 17px;
+  }
+  
+  ul {
     list-style: none;
-}
-ul li a{
-    float:left;
-}
-.icon:hover{
+  }
+  
+  ul li a {
+    float: left;
+  }
+  
+  .icon:hover {
     color: blue;
-}
-</style>
+  }
+  </style>
+  
